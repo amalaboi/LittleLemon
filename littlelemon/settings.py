03019e65f575bd71003e59a799856c8edf27e4a7
Remi Ogunsanya <50562900+amalaboi@users.noreply.github.com>
2025-07-19 21:04:30 +0100

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': 'Orunmilababaifa123',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS':{
